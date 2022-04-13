0e23fdc4f0971162907cf74a0101240fe49d521e
sugimomoto <sugimomoto@gmail.com>
2022-04-14 07:43:08 +0900

    Initial commit

---
+++ src/main/java/sugimomoto/samplecrud/SamplecrudApplication.java
@@ -0,0 +1,13 @@
+package sugimomoto.samplecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplecrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplecrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sugimomoto/samplecrud/SamplecrudApplicationTests.java
@@ -0,0 +1,13 @@
+package sugimomoto.samplecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplecrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
