2c49df677a866b75e88d6e97976618ddad5de64c
sugimomoto <sugimomoto@gmail.com>
2022-04-15 08:00:33 +0900

    Added SleepSummary model

---
+++ src/main/java/sugimomoto/samplecrud/controller/IndexController.java
@@ -0,0 +1,18 @@
+package sugimomoto.samplecrud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("title","helloworld!");
+        return "index";
+    }
+
+}
